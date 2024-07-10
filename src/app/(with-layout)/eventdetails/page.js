80b9eef57988fa99e039f5402caf27b95c5a08ea
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2024-07-11 00:48:16 +0600

    updated branch

--- src/app/(with-layout)/eventdetails/page.js
+++ src/app/(with-layout)/eventdetails/page.js
@@ -67,9 +67,6 @@ const EventDetails = () => {
                                     </p>
                                 </div>
 
-
-
-
                             </div>
                         </div>
 
