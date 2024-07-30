7345586e9e92907f749e2886db6afca82c3126bb
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2024-07-31 01:07:25 +0600

    categories updated

--- src/app/travel-gadgets/components/Shop/AllCategories/AllCategories.js
+++ src/app/travel-gadgets/components/Shop/AllCategories/AllCategories.js
@@ -1,39 +1,72 @@
 'use client';
+
 import { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import SwiperCore, { Autoplay } from 'swiper';
 import Image from 'next/image';
+import Link from 'next/link';
+
+SwiperCore.use([Autoplay]);
 
 const AllCategories = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        // Fetch data from categories.json
-        fetch('/categories.json')
-            .then(response => response.json())
-            .then(data => setCategories(data))
-            .catch(error => console.error('Error fetching categories:', error));
+        const fetchCategories = async () => {
+            try {
+                const response = await fetch('/categories.json');
+                const data = await response.json();
+                setCategories(data);
+            } catch (error) {
+                console.error('Failed to fetch categories:', error);
+            }
+        };
+
+        fetchCategories();
     }, []);
 
     return (
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-10">
-            <div className="container mx-auto m-12 space-y-4">
-                <p className="subtitle text-center">Choose your desired Collection</p>
-                <h1 className="title text-center">Shop By Category</h1>
-            </div>
-            <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-8">
-                {categories.slice(0, 8).map((category, index) => (
-                    <div key={index} className="relative overflow-hidden group">
-                        <Image
-                            src={category.image}
-                            alt={category.title}
-                            layout="responsive"
-                            width={200}
-                            height={200}
-                            className="object-cover transition-transform duration-500 ease-in-out transform group-hover:scale-110 rounded-md"
-                        />
-                        
-                    </div>
+        <div className="container mx-auto mb-10 px-4 sm:px-6 lg:px-8 py-10">
+            <Swiper
+                spaceBetween={30}
+                slidesPerView={1}
+                breakpoints={{
+                    640: { slidesPerView: 1 },
+                    768: { slidesPerView: 2 },
+                    1024: { slidesPerView: 3 },
+                    1440: { slidesPerView: 4 }
+                }}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false
+                }}
+                loop={true}
+            >
+                {categories.map((category, index) => (
+                    <SwiperSlide key={index}>
+                        <div className="flex flex-col items-center justify-between bg-gray-200 pt-5">
+                            <Link href={category.category} legacyBehavior>
+                                <a className="text-xl md:text-lg  font-bold poppins mb-2">{category.title}</a>
+                            </Link>
+                            <div className="border-b border-sky-900 w-1/2 mb-2"></div>
+                            <p className="text-sm text-gray-600 py-5">{category.sub_category}</p>
+                            <div className="relative w-full h-48">
+                                <Image
+                                    src={category.image}
+                                    alt={category.title}
+                                    width={200} // or appropriate size
+                                    height={400} // or appropriate size
+                                    className="object-cover w-full h-full"
+                                />
+                            </div>
+                        </div>
+                    </SwiperSlide>
                 ))}
-            </div>
+            </Swiper>
         </div>
     );
 };
