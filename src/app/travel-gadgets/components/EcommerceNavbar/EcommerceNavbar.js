7a041ccdbedc722909c9023e0f0a2dadb8ca50ab
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2024-09-16 01:29:49 +0600

    hfhf

--- src/app/travel-gadgets/components/EcommerceNavbar/EcommerceNavbar.js
+++ src/app/travel-gadgets/components/EcommerceNavbar/EcommerceNavbar.js
@@ -34,6 +34,7 @@ const EcommerceNavbar = () => {
         };
     }, []);
 
+    // 
     return (
         <nav className="bg-gray-300 border-b fixed w-full z-50 ">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
